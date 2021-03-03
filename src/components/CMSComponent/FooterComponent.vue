c483762c41cffc1cfbd5121e89401696910e0a3b
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-03-03 14:45:24 -0500

    create FooterComponent

---
+++ src/components/CMSComponent/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+   <v-footer
+       padless
+      :outlined="true"
+      :elevation="24"
+   >
+     <v-col
+         class="text-center"
+         cols="12"
+
+     >
+       <strong> <v-icon style="color: black;" :size="18">mdi-copyright</v-icon> Saeedi Trading </strong>
+     </v-col>
+   </v-footer>
+ </div>
+</template>
+
+<script>
+export default {
+name: "FooterComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
