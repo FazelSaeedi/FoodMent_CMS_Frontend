e9a3f82458630f3a20fa801da5557082d6c9f09b
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-03-03 22:09:17 -0500

    Create && implement add get edit Maingroup in MainGroupModul

---
+++ src/store/MainGroupModul.js
@@ -0,0 +1,136 @@
+const axios = require('axios');
+
+export default {
+    namespaced: true,
+    state: {
+
+        mainGroups   : {},
+
+    },
+    getters : {
+
+        getmainGroups : (state) => {
+            return state.mainGroups
+        },
+
+    },
+    actions: {
+
+        retrivemainGroups : (context ) => {
+
+            return new Promise((resolve , reject ) =>{
+                axios.get('http://kalament.ir/api/v1/category/getmainGroupstable/1000' ,{
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                    }
+                }).then(res => {
+                    console.log(res.data.data.data)
+                    resolve(res.data.data.data)
+                    context.commit('retrivemainGroups' , res.data.data.data)
+                }).catch(err => {
+                    console.log(err.Status)
+                    reject(err.response.status)
+                })
+            })
+
+        },
+
+
+        deletemainGroup: ( context , mainGroup ) => {
+            return  new Promise((resolve , reject ) =>{
+                axios.post('https://www.kalament.ir/api/v1/category/deletemainGroup' ,{
+                    id    :   mainGroup.id ,
+                },{
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                    }
+                }).then(res => {
+                    console.log(res)
+                    resolve(res)
+                }).catch(err => {
+                    console.log(err.response)
+                    reject(err)
+                })
+            })
+        },
+
+
+        editmainGroup : (context , mainGroup ) => {
+
+            return new Promise((resolve , reject ) =>{
+                axios.post('https://www.kalament.ir/api/v1/category/editmainGroup' ,{
+                    id    :   mainGroup.id ,
+                    name  :   mainGroup.name,
+                    code  :   mainGroup.code
+                },{
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                    }
+                })
+                    .then(res => {
+                    console.log(res)
+                    resolve(res)
+                })
+                    .catch(err => {
+                    console.log(err.response)
+                    reject(err)
+                })
+            })
+        },
+
+
+        addmainGroup : (context , mainGroup) => {
+
+            return new Promise((resolve , reject ) =>{
+                axios.post('https://www.kalament.ir/api/v1/category/addmainGroup' ,{
+                    name  :   mainGroup.name,
+                    code  :   mainGroup.code
+                },{
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                    }
+                }).then(res => {
+                    console.log(res)
+
+                    resolve(res)
+                }).catch(err => {
+                    console.log(err.response)
+                    reject(err)
+                })
+            })
+        }
+    },
+    mutations: {
+
+
+        retrivemainGroups  : (state , mainGroups) => {
+            state.mainGroups = mainGroups
+        },
+
+
+        //todo : implement delete mainGroup into store
+        deletemainGroup : (state , mainGroup) => {
+            console.log(state + mainGroup)
+        },
+
+
+        //todo : implement edit mainGroup into store
+        editmainGroup : (state , mainGroup) => {
+            console.log(state + mainGroup)
+        },
+
+
+        //todo : implement add mainGroup into store
+        addmainGroup : (state , mainGroup) => {
+            console.log(state + mainGroup)
+        },
+    }
+}
