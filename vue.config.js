34850f526b2b71ea213e5e5ab567ffc372ba2a65
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-03-04 13:38:36 -0500

    set config for deploy on website

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/dist/'
+      : '/',
+  productionSourceMap: false
 }
